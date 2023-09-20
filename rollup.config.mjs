ff4f132c4a01948897c1c8d8ce91c52cef6ba29f
Shivam Mishra <scm.mymail@gmail.com>
2023-09-20 16:50:25 +0530

    feat: add rollup build

---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+import { babel } from "@rollup/plugin-babel";
+
+export default {
+  input: "./src/index.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.es.js",
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  plugins: [babel()],
+};
